cfee00a5b325e523e133dde626139c8ef573b755
Renan Amancio <renan.henriqueba@gmail.com>
2026-09-18 10:40:29 -0300

    Conexão com a a API e inserção de dados com JSON Server

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,12 +5,17 @@ function Select({text, name, options, handleOnChange, value}) {
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
             <select
-            name={name}
-            id={name}
-            onChange={handleOnChange}
-            value={value || ''}
+                name={name}
+                id={name}
+                onChange={handleOnChange}
+                value={value || ''}
             >
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     )
