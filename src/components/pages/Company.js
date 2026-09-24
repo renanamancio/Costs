88bc66f6660495a9657e915862cb7064f3e29ecf
Renan Amancio <renan.henriqueba@gmail.com>
2026-09-24 11:13:45 -0300

    Otimização da função addData()

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,55 +1,48 @@
-import styles from './Company.module.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Button from '../layout/Button'
+import styles from './Company.module.css'
 
 function Company() {
 
     const [days, setDays] = useState([
-        { id: 'dom', text: 'Domingo', style:'default', selected:false},
-        { id: 'seg', text: 'Segunda', style:'default', selected:false},
-        { id: 'ter', text: 'Terça', style:'default', selected:false},
-        { id: 'qua', text: 'Quarta', style:'default', selected:false},
-        { id: 'qui', text: 'Quinta', style:'default', selected:false},
-        { id: 'sex', text: 'Sexta', style:'default', selected:false},
-        { id: 'sab', text: 'Sábado', style:'default', selected:false}
+        { id: 'dom', text: 'Domingo', selected: false },
+        { id: 'seg', text: 'Segunda', selected: false },
+        { id: 'ter', text: 'Terça', selected: false },
+        { id: 'qua', text: 'Quarta', selected: false },
+        { id: 'qui', text: 'Quinta', selected: false },
+        { id: 'sex', text: 'Sexta', selected: false },
+        { id: 'sab', text: 'Sábado', selected: false }
     ])
 
     const daySelected = days.filter(day => day.selected).map(day => day.id)
 
-    function addData(id){
-        const dayId = days.find( d => d.id === id)
-        if(dayId && dayId.selected) {
-            setDays(days.map(day => day.id === id ? {...day, selected: false} : day))
-           // setDaySelected(daySelected.filter(dia => dia !=id))
-        } else{
-            setDays(days.map(day => day.id === id ? {...day, selected: true} : day))
-           // setDaySelected([...daySelected, id])
-        }
+    function addData(id) {
+        setDays(days.map(day => day.id === id ? { ...day, selected: !day.selected } : day))
     }
 
 
-    
+
     return (
         <div className={"flex flex-col gap-20"}>
-            <h1 className={"flex mb-8"}>Escolha os dias de funcionamento da empresa</h1>            
+            <h1 className={"flex mb-8"}>Escolha os dias de funcionamento da empresa</h1>
             <div className={"flex gap-2 row-4"}>
-                {days.length> 0 &&
+                {days.length > 0 &&
                     days.map((day, index) => (
-                        <Button 
+                        <Button
                             key={index}
                             type={day.selected}
                             text={day.text}
                             onClick={() => addData(day.id)}
                         />
-                ))}
+                    ))}
             </div>
             <div className={styles.select}>
                 <p>A empresa funcionará nos seguintes dias: </p>
                 <span>{daySelected.join('-')}</span>
             </div>
-            
+
         </div>
-        )
+    )
 }
 
 export default Company;
